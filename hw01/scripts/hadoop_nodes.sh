a0e9b0f7a8991d6d64f4f5905082c673db9e2ac6
Aleksei Pozdniakov <vokayndzop@ydb.tech>
2025-10-08 15:24:28 +0300

    add hw01

---
+++ hw01/scripts/hadoop_nodes.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -eux
+
+tar -zxf hadoop-3.4.0.tar.gz
---
+++ hw01/templates/hadoop-env.sh
@@ -0,0 +1,4 @@
+export LANG=en_US.UTF-8
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export HADOOP_HOME=/home/hadoop/hadoop-3.4.0
+export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$PATH
